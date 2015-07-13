a2b7de081715d249db5f4f8b75b9120002670012
lbatalha <lfrb25@gmail.com>
2015-07-13 23:57:41 +0100

    replace lsblk with smartctl --scan

--- pysmartnotify.py
+++ pysmartnotify.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
-import os, subprocess, collections, smtplib, socket, json
+import os, subprocess, collections, smtplib, socket
 from email.mime.text import MIMEText
 
 flagged = collections.Counter()
@@ -9,22 +9,25 @@ flagged = collections.Counter()
 check_ids = [1, 5, 7, 10, 11, 196, 197, 198]
 
 #File to store list of flagged drives
-flagged_path = "flagged_drives.list"
+flagged_path = "/tmp/flagged_drives.list"
 
-#List of ignored drives (floppy et al)
-ignored = ['fd0']
+#List of ignored drives
+ignored = ['/dev/fd0']
+
+#Number of recurring errors before no more notifications are sent
+error_threshold = 3
 
 #List of recipient email addresses
 smtp_server = 'smtp.gmail.com'
 USERNAME = ''
 PASSWORD = ''
 
-recipients = ['']
-sender = ''
+recipients = ['user@email.com']
+sender = 'something@something.com'
 
 
-#Handle writing and reading list of flagged drives
 def flagged_storage(op):
+	"""Handle writing and reading list file of flagged drives."""
 	if op == 'w':
 		f = open(flagged_path, op)
 		for k, v in flagged.items():
@@ -45,8 +48,8 @@ def flagged_storage(op):
 	else:
 		return(1)
 
-#Checks each line of SMART attributes for problems and flags them
 def error_parser(drive, line):
+	"""Checks each line of SMART attributes for problems and flags them."""
 	problem = 0
 	if any(int(line['ID#']) == x for x in check_ids):
 		try:
@@ -54,15 +57,15 @@ def error_parser(drive, line):
 				problem = 1
 		except ValueError:
 			print("RAW_VALUE not INT, ignoring.")
-	
+
 		if line['WHEN_FAILED'] != '-':
 			problem = 1
 		if problem == 1:
 			flagged[drive] += 1
 
-def notify(content):
-	body = json.dumps(content, indent=4)
-	msg = MIMEText(body, 'plain')
+def mail(content):
+	"""Send email with flagged drives counter"""
+	msg = MIMEText("Error count per drive: " + str(content), 'plain')
 	msg['Subject'] = "SMART Error detected on %s" % socket.getfqdn()
 	msg['From'] = sender
 	msg['To'] = ", ".join(recipients)
@@ -72,15 +75,17 @@ def notify(content):
 	s.sendmail(sender, recipients, msg.as_string())
 	s.close()
 	return(0)
-	
+
 def main():
 	data = collections.OrderedDict()
-	drives = subprocess.check_output(["lsblk", "-nlS", "-o", "NAME"]).split('\n')[:-1]
+	drives = subprocess.check_output(["sudo", "smartctl", "--scan"]).split('\n')[:-1]
 	if flagged_storage('r') != 0:
-		print("File %s does not exist yet.\n" % flagged_path)
+		print("File %s does not exist yet, creating." % flagged_path)
+	notify = 0
 	for i in drives:
-		if ignored.count(i) == 0:
-			smartdata = subprocess.check_output(["sudo", "smartctl", "-A", "/dev/" + i])
+		i = i.split()
+		if ignored.count(i[0]) == 0:
+			smartdata = subprocess.check_output(["sudo", "smartctl", "-A", i[0]])
 			smartdata = smartdata.split('\n')
 
 			lines = []
@@ -88,18 +93,20 @@ def main():
 			for l in smartdata[7:-2]:
 				line = collections.OrderedDict(zip(labels, l.split()))
 				lines.append(line)
-				error_parser(i, line)
-			data["/dev/" + i] = lines
+				error_parser(i[0], line)
+			data[i[0]] = lines
 		else:
-			print("%s is in ignore list, skipping." % i)
+			print("%s is in ignore list, skipping." % i[0])
+
+		if 0 < flagged[i[0]] <= error_threshold:
+			notify = 1
+	if notify == 1:
+		if mail(flagged) != 0:
+			print("Failed to send notification email")
 	print(flagged)
 	if flagged_storage('w') != 0:
 		print("Failed writing %s" % flagged_path)
 
-
-	if notify(data) != 0:
-		print("Failed to send notification type: %s" % notify_method)
-		
 main()
 
 exit(0)
